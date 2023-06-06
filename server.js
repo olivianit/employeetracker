db2c4454c4a438e3b7d3d67ec426c6c1d664fc42
Aivi <olivianitzel@gmail.com>
2023-06-05 22:45:19 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+// Import and require
+const figlet = require('figlet');
+const colors = require('colors/safe');
+colors.enable();
+// if user wants to disable colors
+// colors.disable();
+
+const db = require('./config/connection');
+const initialPrompt = require('./lib/initialPrompt');
+
+
+// Connect to MySQL
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("MySQL is connected");
+  console.log(
+    colors.gray(
+      `==========================================================================`
+    )
+  );
+  console.log(``);
+  console.log(colors.rainbow(figlet.textSync('            Employee')));
+  console.log(colors.rainbow(figlet.textSync('                Tracker')));
+  console.log(``);
+  console.log(
+    colors.gray(
+      `==========================================================================`
+    )
+  );
+  console.log(``);
+  initialPrompt();
+});
